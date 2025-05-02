8a5f1c0ea15f4821d8ba50385a0c01797aa6032f
pengYuJun <1375841038@qq.com>
2025-05-02 20:08:33 +0800

    add：添加异常处理

---
+++ online-judge-system/src/main/java/com/example/ojs/exception/BusinessException.java
@@ -0,0 +1,35 @@
+package com.example.ojs.exception;
+
+import com.example.ojs.common.ResponseCode;
+import lombok.Getter;
+
+/**
+ * 自定义异常类
+ *
+ * @author pengYuJun
+ */
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final long code;
+
+    private final String message;
+
+    public BusinessException(long code, String message) {
+        super(message);
+        this.code = code;
+        this.message = message;
+    }
+
+    public BusinessException(ResponseCode responseCode) {
+        super(responseCode.getMessage());
+        this.code = responseCode.getCode();
+        this.message = responseCode.getMessage();
+    }
+
+    public BusinessException(ResponseCode responseCode, String message) {
+        super(responseCode.getMessage());
+        this.code = responseCode.getCode();
+        this.message = message;
+    }
+}
---
+++ online-judge-system/src/main/java/com/example/ojs/exception/ThrowUtils.java
@@ -0,0 +1,44 @@
+package com.example.ojs.exception;
+
+import com.example.ojs.common.ResponseCode;
+
+/**
+ * 抛异常工具类
+ *
+ * @author pengYuJun
+ */
+public class ThrowUtils {
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition
+     * @param runtimeException
+     */
+    public static void throwIf(boolean condition, RuntimeException runtimeException) {
+        if (condition) {
+            throw runtimeException;
+        }
+    }
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition
+     * @param errorCode
+     */
+    public static void throwIf(boolean condition, ResponseCode errorCode) {
+        throwIf(condition, new BusinessException(errorCode));
+    }
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition
+     * @param errorCode
+     * @param message
+     */
+    public static void throwIf(boolean condition, ResponseCode errorCode, String message) {
+        throwIf(condition, new BusinessException(errorCode, message));
+    }
+}
