7b8a71c39b6418760fd54702c18265166051c69b
pengYuJun <1375841038@qq.com>
2025-05-03 17:48:08 +0800

    update：完善请求实体

--- online-judge-system/src/main/java/com/example/ojs/pojo/dto/PageDto.java
+++ online-judge-system/src/main/java/com/example/ojs/pojo/dto/PageDto.java
@@ -1,5 +1,6 @@
 package com.example.ojs.pojo.dto;
 
+import com.example.ojs.constant.CommonConstant;
 import lombok.Data;
 
 /**
@@ -8,11 +9,22 @@ import lombok.Data;
 @Data
 public class PageDto {
     /**
-     *
+     * 当前页号
      */
-    private Long current;
+    private long current = 1;
+
     /**
-     *
+     * 页面大小
      */
-    private Long pageSize;
+    private long pageSize = 10;
+
+    /**
+     * 排序字段
+     */
+    private String sortField;
+
+    /**
+     * 排序顺序（默认升序）
+     */
+    private String sortOrder = CommonConstant.SORT_ORDER_ASC;
 }
---
+++ online-judge-system/src/main/java/com/example/ojs/pojo/dto/question/QuestionEditDto.java
@@ -0,0 +1,52 @@
+package com.example.ojs.pojo.dto.question;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author pengYuJun
+ */
+@Data
+public class QuestionEditDto implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 标签列表
+     */
+    private List<String> tags;
+
+    /**
+     * 题目答案
+     */
+    private String answer;
+
+    /**
+     * 判题用例
+     */
+    private List<JudgeCaseDto> judgeCase;
+
+    /**
+     * 判题配置
+     */
+    private JudgeConfigDto judgeConfig;
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
